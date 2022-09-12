29bd74c22bf421e0285a8900b9b6e9a99628a8ba
Matheus Pedroni <mpedroni77@gmail.com>
2022-09-12 18:53:06 -0300

    create http message parser function

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,39 @@ import net from 'net';
 const port = 3000;
 const hostname = '127.0.0.1';
 
+const blackList = ['www.google.com:443', 'facebook'];
+
+function parseHttpMessage(data: Buffer) {
+  const message = data.toString().split('\r\n\r\n')[0];
+
+  const method = message.split('\r\n')[0].split(' ')[0];
+  const httpVersion = message.split('\r\n')[0].split('HTTP/')[1];
+  const isTLS = method === 'CONNECT';
+  const port = isTLS ? 443 : 80;
+  const host = message.split('Host: ')[1].split('\r\n')[0].split(':')[0];
+
+  return {
+    method,
+    httpVersion,
+    isTLS,
+    port,
+    host,
+    message,
+  };
+}
+
+function isHostBlocked(host: string) {
+  return blackList.some((h) => host.includes(h));
+}
+
 const proxy = net.createServer((client) => {
   client.once('data', (data) => {
-    const message = data.toString().split('\r\n\r\n')[0];
-    const method = message.split('\r\n')[0].split(' ')[0];
-    const version = message.split('\r\n')[0].split('HTTP/')[1];
-    const isTLS = method === 'CONNECT';
-    const port = isTLS ? 443 : 80;
-    const host = message.split('Host: ')[1].split('\r\n')[0].split(':')[0];
+    const { host, message, method, isTLS, port, httpVersion } =
+      parseHttpMessage(data);
 
-    if (
-      message.includes('www.google.com:443') ||
-      message.includes('facebook')
-    ) {
+    if (isHostBlocked(host)) {
       client.write(
-        `HTTP/${version} 401 Unauthorized\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
+        `HTTP/${httpVersion} 401 Unauthorized\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
       );
       client.end();
       return;
@@ -35,7 +53,7 @@ const proxy = net.createServer((client) => {
 
     if (isTLS) {
       client.write(
-        `HTTP/${version} 200 OK\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
+        `HTTP/${httpVersion} 200 OK\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
       );
     } else server.write(data);
 
