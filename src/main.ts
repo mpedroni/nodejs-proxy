2547e4b85060b91f5f89205946d3a0f2cf230ca9
Matheus Pedroni <mpedroni77@gmail.com>
2022-09-12 19:22:51 -0300

    create helper function to build http responses

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import net from 'net';
 const port = 3000;
 const hostname = '127.0.0.1';
 
-const blackList = ['www.google.com:443', 'facebook'];
+const blackList = ['google.com', 'facebook'];
 
 function parseHttpMessage(data: Buffer) {
   const message = data.toString().split('\r\n\r\n')[0];
@@ -28,15 +28,32 @@ function isHostBlocked(host: string) {
   return blackList.some((h) => host.includes(h));
 }
 
+type HttpStatusCode = 200 | 403;
+
+function createHttpResponse(code: HttpStatusCode, httpVersion = '1.1') {
+  const codes: Record<HttpStatusCode, string> = {
+    200: 'OK',
+    403: 'Forbidden',
+  };
+
+  const headers = [
+    `HTTP/${httpVersion} ${code} ${codes[code]}`,
+    `Proxy-Server: nodejs-proxy`,
+    '\r\n',
+  ];
+
+  const response = headers.join('\r\n');
+  return response;
+}
+
 const proxy = net.createServer((client) => {
   client.once('data', (data) => {
     const { host, message, method, isTLS, port, httpVersion } =
       parseHttpMessage(data);
 
     if (isHostBlocked(host)) {
-      client.write(
-        `HTTP/${httpVersion} 401 Unauthorized\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
-      );
+      const response = createHttpResponse(403, httpVersion);
+      client.write(response);
       client.end();
       return;
     }
@@ -52,23 +69,22 @@ const proxy = net.createServer((client) => {
     server.pipe(client);
 
     if (isTLS) {
-      client.write(
-        `HTTP/${httpVersion} 200 OK\r\nProxy-Server: nodejs-proxy\r\n\r\n`,
-      );
+      const response = createHttpResponse(200, httpVersion);
+      client.write(response);
     } else server.write(data);
 
     client.on('error', (err) => {
-      console.error('[Client] ', err);
+      console.error(`[Client] [${err.name}]: `, err.message);
     });
 
     server.on('error', (err) => {
-      console.error('[Server] ', err);
+      console.error(`[Server] [${err.name}]: `, err.message);
     });
   });
 });
 
 proxy.on('error', (err) => {
-  console.error('[Proxy] ', err);
+  console.error(`[Proxy] [${err.name}]: `, err.message);
 });
 
 proxy.listen(port, hostname);
